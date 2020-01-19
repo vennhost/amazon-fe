bedae9c6130dfc579155227b8a79f78717a9989e
vennhost <samogconsult@gmail.com>
2020-01-19 23:01:00 +0100

    Fetch API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Card, CardImg, CardText, CardBody, CardLink,
+    CardTitle, CardSubtitle, Row, Col } from "reactstrap";
 import Slider from "./Slider"
 
 class Home extends React.Component {
@@ -11,7 +12,25 @@ class Home extends React.Component {
             <>
                 <Slider />
                 <Container>
-
+                <Row>
+                {this.state.products && this.state.products.map((product, index) =>
+                    <Col md="4">
+                <Card>
+                  <CardBody>
+                    <CardTitle>{product.name}</CardTitle>
+                    <CardSubtitle>{product.brand}</CardSubtitle>
+                  </CardBody>
+                  <img width="100%" src={product.imageurl} alt="Product Image" />
+                  <CardBody>
+                    <CardText>{product.decription}</CardText>
+                    <CardLink href="#">{product.category}</CardLink>
+                    <CardLink href="#">${product.price}</CardLink>
+                  </CardBody>
+                </Card>
+                </Col>
+                )}
+              </Row>
+           
 
                 </Container>
             </>
@@ -20,7 +39,7 @@ class Home extends React.Component {
     }
 
     componentDidMount = async () => {
-        let res = await fetch("https://localhost:3003/products")
+        let res = await fetch("http://localhost:3003/products")
         let products = await res.json()
 
         console.log(products)
